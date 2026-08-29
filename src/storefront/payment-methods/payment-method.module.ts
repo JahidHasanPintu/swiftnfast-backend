0e3c5b2b599dd1dbc95567a9a7efd1362f15a4f5
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2026-08-29 20:33:13 +0600

    fix: import StorefrontAuthModule in PaymentMethodModule for auth guard dependency

--- src/storefront/payment-methods/payment-method.module.ts
+++ src/storefront/payment-methods/payment-method.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentMethodController } from './payment-method.controller';
 import { PaymentMethodService } from './payment-method.service';
 import { DatabaseSchemasModule } from 'src/database/schemas.module';
+import { StorefrontAuthModule } from '../auth/storefront-auth.module';
 
 @Module({
-  imports: [DatabaseSchemasModule],
+  imports: [DatabaseSchemasModule, StorefrontAuthModule],
   controllers: [PaymentMethodController],
   providers: [PaymentMethodService],
   exports: [PaymentMethodService],
